92e99dc534e2fd7f3c33e925182ba636a9b87dbd
mona <manomona2@hotmail.com>
2022-02-10 16:10:35 +0100

    testes unitarios

--- src/app/navegacao/contador/contador.component.spec.ts
+++ src/app/navegacao/contador/contador.component.spec.ts
@@ -1,25 +1,48 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { ContadorComponent } from './contador.component';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { ContadorComponent } from "./contador.component";
 
 describe('ContadorComponent', () => {
   let component: ContadorComponent;
   let fixture: ComponentFixture<ContadorComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ContadorComponent ]
-    })
-    .compileComponents();
-  });
-
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        ContadorComponent
+      ]
+    });
+
     fixture = TestBed.createComponent(ContadorComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+
+    component.valor = 0;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('Deve incrementar corretamente', () => {
+    component.incrementar()
+    expect(component.valor).toBe(1);
+  });
+
+  it('Deve decrementar corretamente', () => {
+    component.incrementar();
+    expect(component.valor).toBe(1);
+    component.decrementar();
+    expect(component.valor).toBe(0);
+  });
+
+  it('não deve decrementar abaixo do valor permitido', () => {
+    component.incrementar();
+    expect(component.valor).toBe(1);
+    component.decrementar();
+    expect(component.valor).toBe(0);
+    component.decrementar();
+    expect(component.valor).toBe(0);
+  });
+
+  it('não deve incrementar acima do valor permitido', () => {
+    for (let i = 0; i < 200; i++) {
+      component.decrementar();
+    }
+    expect(component.valor).toBe(0);
   });
 });
